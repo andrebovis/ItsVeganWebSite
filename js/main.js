ee5936b20e4da854e1b5c1da288defd8eb5f718f
andre bovis <andre_bovis@hotmail.com>
2025-08-31 03:14:01 -0300

    css finally working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-   
-console.log("O arquivo main.js foi carregado com sucesso!");
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
-    
-    console.log("A página está pronta para interatividade!");
+    const form = document.querySelector('form');
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            const curriculoInput = document.getElementById('curriculo');
+            const file = curriculoInput.files[0];
+            
+            if (file) {
+                const fileName = file.name;
+                const fileExtension = fileName.split('.').pop().toLowerCase();
+                
+                if (fileExtension !== 'pdf' && fileExtension !== 'docx') {
+                    e.preventDefault();
+                    alert('Por favor, anexe um currículo nos formatos PDF ou DOCX.');
+                }
+            } else {
+                e.preventDefault();
+                alert('Por favor, anexe seu currículo.');
+            }
+        });
+    }
 
-  
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navUl = document.querySelector('.main-header nav ul');
+
+    if (menuToggle && navUl) {
+        menuToggle.addEventListener('click', () => {
+            navUl.classList.toggle('active');
+        });
+    }
 });
