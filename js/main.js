41331dda5277468f10cff993973ff7cc038e04d1
andre bovis <andre_bovis@hotmail.com>
2025-08-31 02:43:45 -0300

    add main.js and update style.css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+   
+console.log("O arquivo main.js foi carregado com sucesso!");
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    console.log("A página está pronta para interatividade!");
+
+  
+});
