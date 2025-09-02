fa642e230c18ce783087aa34efc6bd73f991d674
andre bovis <andre_bovis@hotmail.com>
2025-09-02 02:30:45 -0300

    adding pic to menu and fixing menu display problems

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.querySelector('form');
-    if (form) {
-        form.addEventListener('submit', (e) => {
-            const curriculoInput = document.getElementById('curriculo');
-            const file = curriculoInput.files[0];
-            
-            if (file) {
-                const fileName = file.name;
-                const fileExtension = fileName.split('.').pop().toLowerCase();
-                
-                if (fileExtension !== 'pdf' && fileExtension !== 'docx') {
-                    e.preventDefault();
-                    alert('Por favor, anexe um currículo nos formatos PDF ou DOCX.');
-                }
-            } else {
-                e.preventDefault();
-                alert('Por favor, anexe seu currículo.');
-            }
-        });
-    }
-
     const menuToggle = document.querySelector('.menu-toggle');
     const navUl = document.querySelector('.main-header nav ul');
 
